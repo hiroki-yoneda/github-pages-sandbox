0ab7e8ff097abbc816fba8db9e76ad977a089b5b
hiroki-yoneda <hiroki.yoneda@jp.ricoh.com>
2020-10-12 09:56:45 +0900

    Fix

--- parent.js
+++ parent.js
@@ -5,19 +5,23 @@ const receiveMessage = function (event) {
     return;
   }
 
-  const s_time = event.data[1]
+  const s_time = event.data[1];
+  const time = document.getElementById("time");
+  const date = new Date();
+  time.innerHTML = "time " + date.getTime() - s_time + " millisec (web message)";
+
   const reader = new FileReader();
   reader.onload = function (e) {
       const preview = document.getElementById("preview");
-      preview.src = e.target.result
-      preview.width = 100
+      preview.src = e.target.result;
+      preview.width = 100;
 
       // 子から送られてきたデータをロードするまでの時間
-      const time = document.getElementById("time");
-      const date = new Date();
-      time.innerHTML = date.getTime() - s_time + " millisec"
+      const load_time = document.getElementById("load_time");
+      const load_date = new Date();
+      load_time.innerHTML = "time " + load_date.getTime() - s_time + " millisec (onload)";
   }
-  reader.readAsDataURL(event.data[0][0])
+  reader.readAsDataURL(event.data[0][0]);
 
   // 選択したファイルのサイズ(byte)を表示
   const size = document.getElementById("size");
